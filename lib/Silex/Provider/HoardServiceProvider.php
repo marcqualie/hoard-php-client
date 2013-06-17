ff2a32016d08f160a3d1f5e33a6f1fcfd906a782
Carl Markham <doddsey65@hotmail.com>
2013-06-18 00:32:24 +0100

    Added Silex Service Provider. Closes #1

---
+++ lib/Silex/Provider/HoardServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Silex\Provider;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use Hoard\Client as HoardClient;
+
+class HoardServiceProvider implements ServiceProviderInterface
+{
+
+    public function register(Application $app)
+    {
+        $app['hoard'] = $app->share(
+            function () use ($app) {
+
+                // Verify Options
+                if (! isset($app['hoard.server'])) {
+                    throw new Exception('Invalid server configuration.');
+                }
+                if (! isset($app['hoard.apikey'])) {
+                    throw new Exception('A valid API Key must be provided.');
+                }
+                if (! isset($app['hoard.bucket'])) {
+                    throw new Exception('A valid bucket ID must be suplied.');
+                }
+
+                $instance = new HoardClient(
+                    array(
+                        'server' => $app['hoard.server'],
+                        'apikey' => $app['hoard.apikey']
+                    )
+                );
+
+                return $instance->getBucket($app['hoard.bucket']);
+            }
+        );
+    }
+
+    public function boot(Application $app)
+    {
+    }
+}
